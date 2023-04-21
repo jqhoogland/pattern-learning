d538e518c81e6c7e6a67b8640eddd2cf6cb69201
Jesse Hoogland <jessequinten@gmail.com>
2023-04-21 11:38:51 +0200

    ADD misc grokking fixes/simplifications

---
+++ grokking/dataset.py
@@ -0,0 +1,177 @@
+# Copied from [openai's implementation](https://github.com/openai/grok/blob/main/grok/data.py)
+
+import itertools
+import math
+import warnings
+from typing import Dict, List, Literal, Optional, Tuple, TypedDict, Union
+
+import blobfile as bf
+import numpy as np
+import torch
+from mod import Mod
+from sympy.combinatorics.permutations import Permutation
+from torch import LongTensor, Tensor
+from torch.utils.data import Dataset
+
+DEFAULT_MODULUS = 97
+DEFAULT_DATA_DIR = "data"
+
+Operator = Literal["/", "*", "+", "-"]
+
+class ModularArithmetic(Dataset):
+    """A Dataset of modular arithmetic equations.
+    
+    Each example is a tuple of the form (i, j, k) where i, j, and k are 
+    integers representing the two operands and result.
+
+    TODO: add support multiple operators/operands
+    TODO: include a label for the operator.
+    TODO: add ability to render/read equations as strings.
+    """
+    class Metadata(TypedDict, total=False):
+        operator: Operator
+        modulus: int
+        frac_label_noise: float
+        seed: int
+        shuffle: bool
+        train: Optional[bool]
+
+    def __init__(self, data: Optional[torch.Tensor]=None, labels: Optional[torch.Tensor]=None, root: Optional[str]=None, metadata: Metadata={}) -> None:
+        """
+        :param train: if true, creates a training dataset, otherwise creates a validation dataset.
+
+        """
+        if root is not None:
+            data, labels = self.load_data(root=root)
+        if data is None or labels is None:
+            raise ValueError("Must provide either data or root")
+
+        self.data = data
+        self.labels = labels
+        self.root = root
+        self.metadata = metadata
+    
+    @classmethod
+    def generate(
+        cls,
+        operator: Operator = "+",
+        modulus: int = DEFAULT_MODULUS,  
+        frac_label_noise: float = 0.0,
+        seed: int = 0,          
+        shuffle: bool = True,
+    ):
+        """
+        Generates a dataset of modular arithmetic equations.
+        
+        :param operator: the operator to use in the equations
+        :param modulus: the modulus to use in the equations
+        :param frac_label_noise: the fraction of labels to flip
+        :param seed: the random seed to use
+        :param shuffle: if true, shuffles the data
+        :returns: a dataset of modular arithmetic equations
+        """
+        torch.manual_seed(seed)
+
+        def apply(i, j, operator, modulus):
+            if operator == "+":
+                return (i + j) % modulus
+            elif operator == "-":
+                return (i - j) % modulus
+            elif operator == "*":
+                return (i * j) % modulus
+            elif operator == "/":
+                return (i * j ** (modulus - 2)) % modulus
+            else:
+                raise ValueError(f"Unknown operator {operator}")
+
+        data = torch.tensor(
+            [(i, j, modulus) for i in range(modulus) for j in range(modulus)],
+            dtype=torch.long,
+        )
+        labels = torch.tensor(
+            [apply(i, j, operator, modulus) for i, j in data[:, :2]],
+            dtype=torch.long,
+        )
+
+        # Apply label noise
+        if frac_label_noise > 0:
+            num_noise = int(frac_label_noise * len(data))
+            noise_from = torch.randperm(len(data))[:num_noise]
+            noise_to = noise_from.roll(1)
+            labels[noise_from, 2] = labels[noise_to, 2]
+
+        if shuffle:
+            permutation = torch.randperm(len(data))
+            data = data[permutation]
+            labels = labels[permutation]
+
+        metadata = cls.Metadata(operator=operator, modulus=modulus, frac_label_noise=frac_label_noise, seed=seed, shuffle=shuffle)
+
+        return cls(data=data, labels=labels, metadata=metadata)
+    
+    def split(self, frac_train: float = 0.8):
+        """
+        Splits the dataset into a training and validation dataset.
+
+        This does not shuffle the data, so the first `frac_train` of the data
+        will be used for training and the rest for validation.
+
+        :param frac_train: fraction of data to use for training
+        """
+        train_len = int(len(self.data) * frac_train)
+
+        train_metadata = self.metadata.copy() | {"train": True}
+        val_metadata = self.metadata.copy() | {"train": False}
+
+        return (
+            ModularArithmetic(self.data[:train_len], self.labels[:train_len], metadata=train_metadata), 
+            ModularArithmetic(self.data[train_len:], self.labels[train_len:], metadata=val_metadata)
+        )
+
+    @classmethod
+    def generate_split(
+        cls,        
+        operator: Operator = "+",
+        modulus: int = DEFAULT_MODULUS,  
+        frac_label_noise: float = 0.0,
+        seed: int = 0,          
+        shuffle: bool = True,
+        frac_train: float = 0.8,
+    ):
+        """
+        A convenience method to generate a modular arithmetic datset and 
+        split it into a training and validation dataset. 
+        
+        See `generate` and `split` for more details.
+        """
+        if shuffle is False:
+            warnings.warn("Shuffling is disabled. This will result in poorly separated training and validation sets.")
+
+        ds = cls.generate(
+            operator=operator,
+            modulus=modulus,
+            frac_label_noise=frac_label_noise,
+            seed=seed,
+            shuffle=shuffle,
+        )
+
+        return ds.split(frac_train=frac_train)
+    
+    def load_data(self, root: str) -> Tuple[Tensor, Tensor]:
+        return torch.load(bf.join(root, "data.pt"))
+    
+    def save_data(self, root: str):
+        torch.save((self.data, self.labels), bf.join(root, "data.pt"))
+
+    def __getitem__(self, index: int) -> Tuple[Tensor, Tensor]:
+        """
+        :param index: the index of the equation
+        :returns: the equation at that index
+        """
+        return self.data[index], self.labels[index]
+    
+    def __iter__(self):
+        return zip(self.data, self.labels)
+    
+    def __len__(self) -> int:
+        return len(self.data)
---
+++ grokking/transformer.py
@@ -0,0 +1,136 @@
+"""
+Based on [Neel Nanda's implementation](https://colab.research.google.com/drive/1F6_1_cWXE5M7WocUcpQWp3v8z4b1jL20).
+"""
+
+from typing import Callable, List, Tuple, Union
+
+import einops
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor, tensor
+
+
+class Embed(nn.Module):
+    def __init__(self, d_vocab: int, d_model: int):
+        super().__init__()
+        self.W_E = nn.Parameter(torch.randn(d_model, d_vocab)/np.sqrt(d_model))
+    
+    def forward(self, x):
+        return torch.einsum('dbp -> bpd', self.W_E[:, x])
+
+
+class Unembed(nn.Module):
+    def __init__(self, d_vocab: int, d_model: int):
+        super().__init__()
+        self.W_U = nn.Parameter(torch.randn(d_model, d_vocab)/np.sqrt(d_vocab))
+    
+    def forward(self, x):
+        return (x @ self.W_U)
+
+
+class PosEmbed(nn.Module):
+    def __init__(self, max_ctx: int, d_model: int):
+        super().__init__()
+        self.W_pos = nn.Parameter(torch.randn(max_ctx, d_model)/np.sqrt(d_model))
+    
+    def forward(self, x):
+        return x+self.W_pos[:x.shape[-2]]
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, d_model: int, epsilon: float = 1e-4):
+        super().__init__()
+        self.w_ln = nn.Parameter(torch.ones(d_model))
+        self.b_ln = nn.Parameter(torch.zeros(d_model))
+        self.epsilon = epsilon
+    
+    def forward(self, x):
+        x = x - x.mean(axis=-1)[..., None]
+        x = x / (x.std(axis=-1)[..., None] + self.epsilon)
+        x = x * self.w_ln
+        x = x + self.b_ln
+        return x
+    
+
+class Attention(nn.Module):
+    mask: Tensor
+
+    def __init__(self, d_model, num_heads, d_head, num_ctx):
+        super().__init__()
+        self.W_K = nn.Parameter(torch.randn(num_heads, d_head, d_model)/np.sqrt(d_model))
+        self.W_Q = nn.Parameter(torch.randn(num_heads, d_head, d_model)/np.sqrt(d_model))
+        self.W_V = nn.Parameter(torch.randn(num_heads, d_head, d_model)/np.sqrt(d_model))
+        self.W_O = nn.Parameter(torch.randn(d_model, d_head * num_heads)/np.sqrt(d_model))
+        self.register_buffer('mask', torch.tril(torch.ones((num_ctx, num_ctx))))
+        self.d_head = d_head
+        
+    def forward(self, x):
+        k = torch.einsum('ihd,bpd->biph', self.W_K, x)
+        q = torch.einsum('ihd,bpd->biph', self.W_Q, x)
+        v = torch.einsum('ihd,bpd->biph', self.W_V, x)
+        attn_scores_pre = torch.einsum('biph,biqh->biqp', k, q)
+        attn_scores_masked = torch.tril(attn_scores_pre) - 1e10 * (1 - self.mask[:x.shape[-2], :x.shape[-2]])
+        attn_matrix = F.softmax(attn_scores_masked/np.sqrt(self.d_head), dim=-1)
+        z = torch.einsum('biph,biqp->biqh', v, attn_matrix)
+        z_flat = einops.rearrange(z, 'b i q h -> b q (i h)')
+        out = torch.einsum('df,bqf->bqd', self.W_O, z_flat)
+        return out
+
+
+class MLP(nn.Module):
+    def __init__(self, d_model: int, d_mlp: int, act_fn: Callable = F.relu):
+        super().__init__()
+        self.W_in = nn.Parameter(torch.randn(d_mlp, d_model)/np.sqrt(d_model))
+        self.b_in = nn.Parameter(torch.zeros(d_mlp))
+        self.W_out = nn.Parameter(torch.randn(d_model, d_mlp)/np.sqrt(d_model))
+        self.b_out = nn.Parameter(torch.zeros(d_model))
+        self.act_fn = act_fn 
+        # self.ln = LayerNorm(d_mlp)
+        
+    def forward(self, x):
+        x = torch.einsum('md,bpd->bpm', self.W_in, x) + self.b_in
+        x = self.act_fn(x)                
+        x = torch.einsum('dm,bpm->bpd', self.W_out, x) + self.b_out
+
+        return x
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, d_model: int, d_mlp: int, d_head: int, num_heads: int, num_ctx: int, act_fn: Callable = F.relu):
+        super().__init__()
+        # self.ln1 = LayerNorm(d_model)
+        self.attn = Attention(d_model, num_heads, d_head, num_ctx)
+        # self.ln2 = LayerNorm(d_model)
+        self.mlp = MLP(d_model, d_mlp, act_fn)
+    
+    def forward(self, x):
+        x = x + self.attn(x)
+        x = x + self.mlp((x))
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(self, num_layers: int, d_vocab: int, d_model: int, d_mlp: int, d_head: int, num_heads: int, num_ctx: int, act_fn: Callable = F.relu, use_ln: bool=True):
+        super().__init__()
+
+        self.embed = Embed(d_vocab, d_model)
+        self.pos_embed = PosEmbed(num_ctx, d_model)
+        self.blocks = nn.ModuleList([
+            TransformerBlock(d_model, d_mlp, d_head, num_heads, num_ctx, act_fn) 
+            for i in range(num_layers)
+        ])
+        # self.ln = LayerNorm(d_model)
+        self.unembed = Unembed(d_vocab, d_model)
+        # self.use_ln = use_ln
+    
+    def forward(self, x):
+        x = self.embed(x)
+        x = self.pos_embed(x)
+        
+        for block in self.blocks:
+            x = block(x)
+        # x = self.ln(x)
+        x = self.unembed(x)
+        return x
